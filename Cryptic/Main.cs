9a1beed29cff13b92a89b1ccd75e79ca5d1151ff
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-26 14:59:22 +0200

    implemented basic flow

--- Cryptic/Main.cs
+++ Cryptic/Main.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("hello world");
+﻿Console.WriteLine("Welcome to CRYPTIC!");
+GiveOptions();
+Console.ReadKey();
+
+void GiveOptions()
+{
+    Console.WriteLine("1. Encrypt");
+    Console.WriteLine("2. Decrypt");
+    Console.WriteLine("3. Quit");
+    HandleOptionInput();
+}
+
+void HandleOptionInput()
+{
+    int choice = Console.ReadKey().KeyChar - '0';
+    Console.WriteLine();
+    
+    switch (choice)
+    {
+        case 3:
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please choose from the above only.");
+            GiveOptions();
+            break;
+    }
+}
